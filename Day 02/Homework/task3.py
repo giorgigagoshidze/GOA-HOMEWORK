51f8bb86fc2e6309c9b62e92a317bf0fe2a44d7d
giorgigagoshidze <g18900437@gmail.com>
2024-06-26 05:09:41 -0700

    third commit

---
+++ Day 02/Homework/task3.py
@@ -0,0 +1,16 @@
+#1
+number1 = 150
+number2 = 320
+print(number1 + number2)
+#N2
+number1 = 920
+number2 = 200
+print(number1 - number2)
+#N3
+number1 = 16
+number2 = 43
+print(number1 * number2)
+#4
+number1 = 1892
+number2 = 4
+print(number1 / number2)
